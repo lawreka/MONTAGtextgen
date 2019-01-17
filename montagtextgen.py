8d4893d26b0bf74665b30971caaa153fe317f929
Kathryn <kathryn@Kathryns-MacBook-Air.local>
2019-01-17 11:45:20 +0100

    added textgenrnn, montag text, and montagtextgen script

---
+++ montagtextgen.py
@@ -0,0 +1,10 @@
+from textgenrnn import textgenrnn
+
+textgen = textgenrnn()
+textgen.train_from_file('montagtext.txt', num_epochs=1)
+output = textgen.generate()
+
+print(output)
+
+with open('montagrnntext.txt', 'w') as file:
+    file.write(output)
